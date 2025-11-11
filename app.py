262dba21b44c7a3e8eb6b8251abdc69ab538e6b7
123Zdenda <zdenda.tancovani@seznam.cz>
2025-11-11 11:19:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ st.title("První divný graf")
 uploaded_file = st.file_uploader("Upload a csv file")
 
 if uploaded_file is not None:
-    try:
-        df = pd.read_csv(uploaded_file, sep = ",")
-        st.dataframe(df)
+    df = pd.read_csv(uploaded_file, sep = ",")
+    st.dataframe(df)
     st.subheader("add date to the plot")
     col1, col2 = st.columns(2)
-      with col1:
-        x_column = st.selectbox("Select x-axis column", df.columns)
-      with col2:
-        
-
-    except Exception as  e:
-        st.error(f"toto je chyba {e}")
+    with col1:
+      x_column = st.selectbox("Select x-axis column", df.columns)
+    with col2:
+      y_column = st.selectbox("Select y-axis column", df.columns)
+    col3, col4, col5 = st.columns(3)
+    with col3:
+      x_label = st.text_input("Name of the x-axis label", value=x_column)
+      y_label = st.text_input("Name of the y-axis label", value=x_column)
